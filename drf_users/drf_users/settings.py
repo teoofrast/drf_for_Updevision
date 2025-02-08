4366d918861f7cf29f5355febb516c1f6b3b3da6
Teoofrast <teoofrast@gmail.com>
2025-02-08 11:42:07 +0500

    some changes in settings.py

--- drf_users/drf_users/settings.py
+++ drf_users/drf_users/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'drf_users.accounts',
+    'accounts.apps.AccountsConfig',
     'rest_framework_simplejwt.token_blacklist',
 ]
 
